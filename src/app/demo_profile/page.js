b388f78d12dc99ca5bc2f59c3631d4f6fb95e926
Augustus Soto <72059498+lordsaviorjesus@users.noreply.github.com>
2023-12-07 12:50:07 -0800

    last work

--- src/app/demo_profile/page.js
+++ src/app/demo_profile/page.js
@@ -1,6 +1,7 @@
 "use client"
 
 import Image from 'next/image';
+<<<<<<< Updated upstream
 import './style.css';
 import * as React from 'react';
 import ImageList from '@mui/material/ImageList';
@@ -26,6 +27,98 @@ export function UploadImageButton() {
     const file = event.target.files[0];
     // Do something with the uploaded file (e.g., send it to a server)
     //itemData.push({image: file, title :})
+=======
+import { useState } from 'react';
+import ImageListItem from '@mui/material/ImageListItem';
+
+import ImageList from '@mui/material/ImageList';
+
+import './style.css';
+
+const itemData = [
+  {
+    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+    title: 'Breakfast',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+    title: 'Burger',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    title: 'Camera',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+    title: 'Coffee',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+    title: 'Hats',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+    title: 'Honey',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+    title: 'Basketball',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+    title: 'Fern',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+    title: 'Mushrooms',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+    title: 'Tomato basil',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+    title: 'Sea star',
+  },
+  {
+    img: 'kevin2.jpg',
+    title: 'Bike',
+  },
+];
+
+export function StandardImageList() {
+  return (
+<ImageList sx={{ width: 1026, height: 1026}} cols={3} rowHeight={342}>
+  {itemData.map((item) => (
+    <ImageListItem key={item.img}>
+      <img
+        srcSet={`${item.img}?w=500&h=500&fit=crop&auto=format&dpr=2 2x`}
+        src={`${item.img}?w=500&h=500&fit=crop&auto=format`}
+        alt={item.title}
+        loading="lazy"
+      />
+    </ImageListItem>
+  ))}
+</ImageList>
+  );
+}
+
+
+
+export default function Profile() {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const petInfo = {
+    name: '...',
+    species: '...',
+    breed: '...',
+    age: '...',
+    vaccineStatus: '...',
+    neuterStatus: '...',
+    location: '...',
+    bio: '...',
+    profileImage: '/goated.jpg', // Example URL for the profile image
+>>>>>>> Stashed changes
   };
   return (
     <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
@@ -59,6 +152,13 @@ const PetInfo = () =>{
   </ul>
   )
 
+<<<<<<< Updated upstream
+=======
+
+  
+
+  const [editedPetInfo, setEditedPetInfo] = useState({ ...petInfo });
+>>>>>>> Stashed changes
 
 
 }
@@ -162,6 +262,10 @@ export default function Profile() {
         <UploadImageButton />
       </div>
 
+<<<<<<< Updated upstream
+=======
+      <StandardImageList />
+>>>>>>> Stashed changes
     </div>
   );
 }
